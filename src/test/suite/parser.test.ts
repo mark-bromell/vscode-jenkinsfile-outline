66403942e755f1f3110aa68168e07f7ddb92bd7a
mark-bromell <33425418+mark-bromell@users.noreply.github.com>
2023-07-14 15:07:45 +0100

    Clean up

--- src/test/suite/parser.test.ts
+++ src/test/suite/parser.test.ts
@@ -20,37 +20,11 @@ suite('Parser test suite', () => {
         assert.strictEqual(symbols[0].children[0].children[0].children.length, 2);
         assert.strictEqual(symbols[0].children[0].children[1].children.length, 0);
         assert.strictEqual(symbols[0].children[1].children.length, 0);
-        
+
         assert.strictEqual(symbols[0].name, 'Pipeline');
         assert.strictEqual(symbols[0].children[0].name, 'Setup');
         assert.strictEqual(symbols[0].children[1].name, 'Static Scan');
         assert.strictEqual(symbols[0].children[2].name, 'Build and Test');
         assert.strictEqual(symbols[0].children[3].name, 'Report');
     });
-
-    // test('Stages count is correct', () => {
-    //     const stages: vscode.DocumentSymbol[] = parseJenkinsfile(jenkinsfileFull);
-    //     assert.strictEqual(stages.length, 4);
-    //     assert.strictEqual(stages[0].children.length, 2);
-    //     assert.strictEqual(stages[0].children[0].children.length, 2);
-    //     assert.strictEqual(stages[2].children.length, 2);
-    // });
-
-    // test('Stages are parsed correctly', () => {
-    //     const stages: vscode.DocumentSymbol[] = parseJenkinsfile(jenkinsfileFull);
-    //     assert.strictEqual(stages.length, 4);
-    //     assert.strictEqual(stages[0].name, 'Setup');
-    //     assert.strictEqual(stages[0].children[0].name, 'Pre-action');
-    //     assert.strictEqual(stages[0].children[0].children[0].name, 'One');
-    //     assert.strictEqual(stages[0].children[0].children[1].name, 'Two');
-    //     assert.strictEqual(stages[0].children[1].name, 'Downloading');
-
-    //     assert.strictEqual(stages[1].name, 'Static Scan');
-
-    //     assert.strictEqual(stages[2].name, 'Build and Test');
-    //     assert.strictEqual(stages[2].children[0].name, 'Build');
-    //     assert.strictEqual(stages[2].children[1].name, 'Test');
-
-    //     assert.strictEqual(stages[3].name, 'Report');
-    // });
 });
