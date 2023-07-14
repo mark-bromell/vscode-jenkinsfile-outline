b23f9e3ce291cc63bd8fe672ca2a904f95de0503
mark-bromell <33425418+mark-bromell@users.noreply.github.com>
2023-07-14 15:06:55 +0100

    Initial implementation working

--- src/parser.ts
+++ src/parser.ts
@@ -12,50 +12,43 @@ export default function parseJenkinsfile(documentText: string): vscode.DocumentS
         parseRuleStage,
     ];
 
-    do {
-        rules.every(rule => !parseRule(lexer, context, rule));
-    } while (lexer.peek().type !== 'EOF');
+    const tokens = lexer.tokens();
 
-    return [];
+    for (let i = 0; i < tokens.length; i++) {
+        for (let j = 0; j < rules.length; j++) {
+            if (parseRule(i, tokens, context, rules[j])) {
+                break;
+            }
+        }
+    }
+
+    return context.symbols;
 }
 
 type ParseRuleFunction = {
-    (token: Token, lexer: Tokenizr, context: ParserContext): boolean;
+    (index: number, tokens: Token[], context: ParserContext): boolean;
 };
 
 function parseRule(
-    lexer: Tokenizr,
+    index: number,
+    tokens: Token[],
     context: ParserContext,
     fn: ParseRuleFunction,
 ): boolean {
-    lexer.begin();
-    const token = lexer.token();
-
-    if (token === null) {
-        lexer.rollback();
-        return false;
-    }
-
     let result: boolean;
 
     try {
-        result = fn(token, lexer, context);
+        result = fn(index, tokens, context);
     } catch (error) {
         console.error(error);
         result = false;
     }
 
-    if (result) {
-        lexer.commit();
-    } else {
-        lexer.rollback();
-    }
-
     return result;
 }
 
-function parseRuleDepthIncrease(token: Token, lexer: Tokenizr, context: ParserContext): boolean {
-    if (!(token.type === 'word' && token.value === '{')) {
+function parseRuleDepthIncrease(index: number, tokens: Token[], context: ParserContext): boolean {
+    if (!(tokens[index].type === 'char' && tokens[index].value === '{')) {
         return false;
     }
 
@@ -63,8 +56,8 @@ function parseRuleDepthIncrease(token: Token, lexer: Tokenizr, context: ParserCo
     return true;
 }
 
-function parseRuleDepthDecrease(token: Token, lexer: Tokenizr, context: ParserContext): boolean {
-    if (!(token.type === 'word' && token.value === '}')) {
+function parseRuleDepthDecrease(index: number, tokens: Token[], context: ParserContext): boolean {
+    if (!(tokens[index].type === 'char' && tokens[index].value === '}')) {
         return false;
     }
 
@@ -72,80 +65,86 @@ function parseRuleDepthDecrease(token: Token, lexer: Tokenizr, context: ParserCo
     return true;
 }
 
-function parseRulePipeline(token: Token, lexer: Tokenizr, context: ParserContext): boolean {
-    if (!(token.type === 'word' && token.value === 'pipeline')) {
+function parseRulePipeline(index: number, tokens: Token[], context: ParserContext): boolean {
+    if (!(tokens[index].type === 'word' && tokens[index].value === 'pipeline')) {
         return false;
     }
 
     let range = new vscode.Range(
-        new vscode.Position(token.line, token.pos),
-        new vscode.Position(token.line, token.pos + token.value.length)
+        new vscode.Position(tokens[index].line, tokens[index].pos),
+        new vscode.Position(tokens[index].line, tokens[index].pos + tokens[index].value.length)
     );
     let selectionRange = new vscode.Range(
-        new vscode.Position(token.line, token.pos),
-        new vscode.Position(token.line, token.pos + token.value.length)
+        new vscode.Position(tokens[index].line, tokens[index].pos),
+        new vscode.Position(tokens[index].line, tokens[index].pos + tokens[index].value.length)
     );
     let newSymbol: vscode.DocumentSymbol = new vscode.DocumentSymbol(
         'Pipeline',
-        '',
-        vscode.SymbolKind.File,
-        range,
-        selectionRange
-    );
-
-    context.push(newSymbol);
-    return true;
-}
-
-function parseRuleStage(token: Token, lexer: Tokenizr, context: ParserContext): boolean {
-    if (!(token.type === 'word' && token.value === 'stage')) {
-        return false;
-    }
-
-    lexer.consume('word', '(');
-    const stageName = lexer.consume('string').value;
-    lexer.consume('word', ')');
-
-    let range = new vscode.Range(
-        new vscode.Position(token.line, token.pos),
-        new vscode.Position(token.line, token.pos + token.value.length)
-    );
-    let selectionRange = new vscode.Range(
-        new vscode.Position(token.line, token.pos),
-        new vscode.Position(token.line, token.pos + token.value.length)
-    );
-    let newSymbol: vscode.DocumentSymbol = new vscode.DocumentSymbol(
-        stageName,
         '',
         vscode.SymbolKind.Interface,
         range,
         selectionRange
     );
 
-    context.push(newSymbol);
+    context.pushBlock(newSymbol);
     return true;
+}
+
+function parseRuleStage(index: number, tokens: Token[], context: ParserContext): boolean {
+    if (!(tokens[index].type === 'word' && tokens[index].value === 'stage')) {
+        return false;
+    }
+
+    const stageName = tokens[index + 2].value;
+
+    let range = new vscode.Range(
+        new vscode.Position(tokens[index].line, tokens[index].pos),
+        new vscode.Position(tokens[index].line, tokens[index].pos + tokens[index].value.length)
+    );
+    let selectionRange = new vscode.Range(
+        new vscode.Position(tokens[index].line, tokens[index].pos),
+        new vscode.Position(tokens[index].line, tokens[index].pos + tokens[index].value.length)
+    );
+    let newSymbol: vscode.DocumentSymbol = new vscode.DocumentSymbol(
+        stageName,
+        'stage',
+        vscode.SymbolKind.Event,
+        range,
+        selectionRange
+    );
+
+    context.pushBlock(newSymbol);
+    return true;
+}
+
+class SymbolDepth {
+    index: number;
+    depth: number;
+
+    constructor(index: number, depth: number) {
+        this.index = index;
+        this.depth = depth;
+    }
 }
 
 class ParserContext {
     symbols: vscode.DocumentSymbol[];
-    depth: number[];
+    symbolDepth: SymbolDepth[];
+    depthCount: number = 0;
 
-    constructor(
-        symbols: vscode.DocumentSymbol[] = [],
-        depth: number[] = [],
-    ) {
-        this.symbols = symbols;
-        this.depth = depth;
+    constructor() {
+        this.symbols = [];
+        this.symbolDepth = [];
     }
 
     get currentSymbol(): vscode.DocumentSymbol | undefined {
-        if (!this.depth) {
+        if (this.symbolDepth.length === 0) {
             return undefined;
         }
 
-        let symbol = this.symbols[this.depth[0]];
-        for (let i = 1; i < this.depth.length; i++) {
-            symbol = symbol.children[this.depth[i]];
+        let symbol = this.symbols[this.symbolDepth[0].index];
+        for (let i = 1; i < this.symbolDepth.length; i++) {
+            symbol = symbol.children[this.symbolDepth[i].index];
         }
         return symbol;
     }
@@ -158,19 +157,36 @@ class ParserContext {
         }
     }
 
+    pushBlock(symbol: vscode.DocumentSymbol) {
+        this.push(symbol);
+        this.pushSymbolDepth();
+    }
+
     pushDepth() {
+        this.depthCount++;
+    }
+
+    popDepth() {
+        this.depthCount--;
+
+        if (!this.symbolDepth.at(-1)) {
+            return;
+        }
+
+        if (this.depthCount === this.symbolDepth.at(-1)!.depth) {
+            this.symbolDepth.pop();
+        }
+    }
+
+    pushSymbolDepth() {
         if (!this.symbols) {
             return;
         }
 
         if (this.currentSymbol) {
-            this.depth.push(this.currentSymbol.children.length - 1);
+            this.symbolDepth.push(new SymbolDepth(this.currentSymbol.children.length - 1, this.depthCount));
         } else {
-            this.depth.push(this.symbols.length - 1);
+            this.symbolDepth.push(new SymbolDepth(this.symbols.length - 1, this.depthCount));
         }
-    }
-
-    popDepth() {
-        this.depth.pop();
     }
 }
--- src/test/suite/parser.test.ts
+++ src/test/suite/parser.test.ts
@@ -12,6 +12,22 @@ suite('Parser test suite', () => {
         assert.strictEqual(symbols[0].name, 'Pipeline');
     });
 
+    test('Full pipeline parsing', () => {
+        const symbols: vscode.DocumentSymbol[] = parseJenkinsfile(jenkinsfileFull);
+        assert.strictEqual(symbols.length, 1);
+        assert.strictEqual(symbols[0].children.length, 4);
+        assert.strictEqual(symbols[0].children[0].children.length, 2);
+        assert.strictEqual(symbols[0].children[0].children[0].children.length, 2);
+        assert.strictEqual(symbols[0].children[0].children[1].children.length, 0);
+        assert.strictEqual(symbols[0].children[1].children.length, 0);
+        
+        assert.strictEqual(symbols[0].name, 'Pipeline');
+        assert.strictEqual(symbols[0].children[0].name, 'Setup');
+        assert.strictEqual(symbols[0].children[1].name, 'Static Scan');
+        assert.strictEqual(symbols[0].children[2].name, 'Build and Test');
+        assert.strictEqual(symbols[0].children[3].name, 'Report');
+    });
+
     // test('Stages count is correct', () => {
     //     const stages: vscode.DocumentSymbol[] = parseJenkinsfile(jenkinsfileFull);
     //     assert.strictEqual(stages.length, 4);
